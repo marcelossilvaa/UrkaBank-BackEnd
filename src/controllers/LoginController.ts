ce70c648c8ea238a19a32632d9e54d5307113be4
Marcelo Silva <marcelo_810@hotmail.com>
2024-04-22 17:33:19 -0300

    ✅ applying jwt

---
+++ src/controllers/LoginController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import { sign } from 'jsonwebtoken'
+
+const user = {
+  id_user: '12345',
+  name: 'Marce',
+  email: 'marce@dio',
+  password: 'password'
+}
+
+export class LoginController {
+  login = async (request: Request, response: Response) => {
+
+    const tokenData = {
+      name: user.name,
+      email: user.email
+    }
+
+    const tokenKey = '123456789'
+
+    const tokenOptions = {
+      subject: user.id_user
+    }
+
+    const token = sign(tokenData, tokenKey, tokenOptions)
+
+    return response.status(200).json({ token })
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
+import { LoginController } from './controllers/LoginController'
 import { Router, Request, Response } from 'express'
 import { UserController } from './controllers/UserController'
 
 export const router = Router()
 
 const userController = new UserController()
+const loginController = new LoginController()
 
 router.post('/user', userController.createUser)
 router.get('/user', userController.getUser)
 router.delete('/user', userController.deleteUser)
+
+router.post('/login', loginController.login)
