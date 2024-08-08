183a298122042f011967d9f329ebfae628b4d44e
Marcelo Silva <marcelo_810@hotmail.com>
2024-08-07 21:38:40 -0300

    ♻️  vercel-build database routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,30 @@
-import { LoginController } from './controllers/LoginController'
-import { Router, Request, Response } from 'express'
-import { UserController } from './controllers/UserController'
+import { LoginController } from './controllers/LoginController';
+import { Router, Request, Response } from 'express';
+import { UserController } from './controllers/UserController';
 
-export const router = Router()
+export const router = Router();
 
-const userController = new UserController()
-const loginController = new LoginController()
+const userController = new UserController();
+const loginController = new LoginController();
 
-router.post('/user', userController.createUser)
-router.get('/user', userController.listUsers)
-router.get('/user/:userId', userController.getUser)
-router.delete('/user', userController.deleteUser)
+router.post('/user', (req: Request, res: Response) => {
+  console.log('POST /user');
+  userController.createUser(req, res);
+});
+router.get('/user', (req: Request, res: Response) => {
+  console.log('GET /user');
+  userController.listUsers(req, res);
+});
+router.get('/user/:userId', (req: Request, res: Response) => {
+  console.log(`GET /user/${req.params.userId}`);
+  userController.getUser(req, res);
+});
+router.delete('/user', (req: Request, res: Response) => {
+  console.log('DELETE /user');
+  userController.deleteUser(req, res);
+});
 
-router.post('/login', loginController.login)
+router.post('/login', (req: Request, res: Response) => {
+  console.log('POST /login');
+  loginController.login(req, res);
+});
