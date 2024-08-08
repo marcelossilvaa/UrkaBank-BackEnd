e4a29b26205831ef4de85d0aa498e8f6d87d1656
Marcelo Silva <marcelo_810@hotmail.com>
2024-08-07 21:38:59 -0300

    ♻️  vercel-build database routes

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import { router } from './routes';
-import 'reflect-metadata';
+import express from 'express';
 import { AppDataSource } from './database';
+import { router } from './routes';
+import cors from 'cors';
 
-const server = express();
+const app = express();
 
-// Usa o middleware cors
-server.use(cors());
+app.use(cors());
+app.use(express.json());
+app.use(router);
 
-server.use(express.json());
-server.use(router);
+const PORT = process.env.PORT || 5000;
 
-server.get('/', (request: Request, response: Response) => {
-  return response.status(200).json({ message: 'DioBank API' });
-});
-
-server.listen(5000, () => console.log('Server on update'));
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error during Data Source initialization:', error);
+  });
